1a7e22ff03484b9a11dec5f25d601546bff06b8c
윤수현 <yoonsuhyeon@yunsuhyeon-ui-MacBookPro.local>
2021-12-14 22:36:55 +0900

    lecture chapter1-1

---
+++ src/lecture/chapter1/Test.java
@@ -0,0 +1,10 @@
+package lecture.chapter1;
+
+/**
+ * 테스트를 위해서 생성 (주석)
+ */
+public class Test {
+    public static void main(String... args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/lecture/chapter1/Test2.java
@@ -0,0 +1,25 @@
+package lecture.chapter1;
+
+/**
+ * 변수 ,타입 , 리터럴
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        //정수형 타입 int  변수명은 numberA  : int 타입의 numberA라는 변수를 선언
+        //10이라는 숫자는 리터럴 이라고 한다  리터럴은 변수에 대입 하는 값을 구체적 으로 표현한 것.
+        int numberA;
+        //numberA에 10 을 대입
+        numberA = 10;
+        //?????
+        int numberB = 10;
+
+        System.out.println(numberA);
+        System.out.println(numberB);
+
+        String s = "string";
+        boolean bool = true;
+
+        System.out.println(s);
+        System.out.println(bool);
+    }
+}
