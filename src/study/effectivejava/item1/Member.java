775a212240e41ee48075861e35abcb13a710ff1f
YoonSuHyeon <96-05-03@naver.com>
2022-02-13 17:14:55 +0900

    Static Factory Method 코드작성으로 장점과 단점 확인(1)

---
+++ src/study/effectivejava/item1/Member.java
@@ -0,0 +1,36 @@
+package study.effectivejava.item1;
+
+/**
+ * Static Factory Method Example
+ */
+public class Member {
+    private String name;
+    private int age;
+    private MemberType type;
+
+    public Member(String name, int age, MemberType type) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+    }
+
+    public static Member admin(String name, int age) {
+        return new Member(name, age, MemberType.ADMIN);
+    }
+
+    private enum MemberType {
+        ADMIN, DEFAULT
+    }
+
+    public static void main(String[] args) {
+        String name = "TestName";
+        int age = 20;
+        MemberType type = MemberType.ADMIN;
+
+        //생성자를 통해서 객체 생성
+        Member member = new Member(name, age, type);
+        //Static Factory Method 사용
+        Member admin = Member.admin(name, age);
+
+    }
+}
